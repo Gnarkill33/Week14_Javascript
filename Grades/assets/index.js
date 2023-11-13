2b4ad67077abe5c82e015f4e031dab32f42531a6
Gnarkill33 <gnarkill33@gmail.com>
2023-11-13 20:43:37 +0300

    fifth commit on JS

--- Grades/assets/index.js
+++ Grades/assets/index.js
@@ -8,82 +8,107 @@ console.log(grades);
 
 // Выводим средний балл студентов
 
-// ?????????????????????????????????????????????????????????/ 
-// вопрос по length
-
-const getAverageGrade = () => {
+const getAverageGrade = (grades) => {
     let sum = grades.reduce((sum, current) => sum + current, 0); 
     let length = grades.length;
     console.log(Math.round(sum / length));
 };
-getAverageGrade();
+getAverageGrade(grades);
 
 // Выводим максимальный балл среди студентов
 
-const getHighestGrade = () => { 
+const getHighestGrade = (grades) => { 
     const gradesSorted = grades.sort(function(a, b) {
     return (a - b)});
     console.log(gradesSorted[gradesSorted.length - 1]);
     console.log(gradesSorted);
 };
-getHighestGrade();
+getHighestGrade(grades);
 
 // Выводим минимальный балл среди студентов
 
-const getLowestGrade = () => { 
+const getLowestGrade = (grades) => { 
     const gradesSorted = grades.sort(function(a, b) {
     return (b - a)});
     console.log(gradesSorted);
     console.log(gradesSorted[gradesSorted.length - 1]);
 };
-getLowestGrade();
+getLowestGrade(grades);
 
 // Выводим кол-во студентов с положительной оценкой (>= 60)
 // Выводим кол-во оставшихся оценок
 
-const showGoodStudents = () => {
+const showGoodStudents = (grades) => {
     const goodStudents = grades.filter(item => item >= 60);
     console.log(goodStudents.length);
     const otherStudents = grades.length - goodStudents.length;
     console.log(otherStudents);
 };
-showGoodStudents();
+showGoodStudents(grades);
 
 // Выводим кол-во студентов с отрицательной оценкой (< 60)
 // Выводим кол-во оставшихся оценок 
 
-const showBadStudents = () => {
+const showBadStudents = (grades) => {
     const badStudents = grades.filter(item => item < 60);
     console.log(badStudents.length);
     const restOfStudents = grades.length - badStudents.length;
     console.log(restOfStudents);
 };
-showBadStudents();
+showBadStudents(grades);
 
 // Преобразуем числовые оценки в буквенные (формат A/B/C/D/E)
 
 
 const showLetterGrades = (grades) => {
-    switch (grades) {
-        case (80 >= grades <= 100):
-            grades = "A";
-            break;
-        case (60 >= grades <= 79):
-            grades = "B";
-            break;
-        case (40 >= grades <= 59):
-            grades = "C";
-            break;
-        case 20 >= grades <= 39:
-            grades = "D";
-            break;
-        case grades < 20:
-            grades = "E";
-            break;
+    const letterGrades = grades.map(function (grade) {
+        switch (grade) {
+        case (80 <= grade && grade <= 100):
+        return "A";
+        // break;
+        case (60 <= grade && grade <= 79):
+        return "B";
+        // break;
+        case (40 <= grade && grade <= 59):
+        return "C";
+        // break;
+        case (20 <= grade && grade <= 39):
+        return "D";
+        // break;
+        case (grade < 20):
+        return "E"
+        // break;
         default:
-            grades = "Undefined";
-}
-console.log(grades);
-}
-showLetterGrades();
+        return "Error"
+        }}
+    )
+    console.log(letterGrades);
+    }
+    showLetterGrades(grades);
+
+
+
+    //     switch (grade) {
+    //         case 80 >= grade <= 100:
+    //             return "A";
+    //             break;
+    //         case 60 >= grade <= 79:
+    //             return "B";
+    //             break;
+    //         case 40 >= grade <= 59:
+    //             return "C";
+    //             break;
+    //         case 20 >= grade <= 39:
+    //             return "D";
+    //             break;
+    //         case grade < 20:
+    //             return "E";
+    //             break;
+    //         default:
+    //             return "Undefined";
+    // }
+//     })
+// console.log(letterGrades);
+// // }
+// // showLetterGrades(grades);
 
